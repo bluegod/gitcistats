76a358eddae50811f59d7c9c30bdd93c261c72a2
James Lopez <james.lopez@imperatives.co.uk>
2014-10-16 20:43:48 +0100

    Added 1st version of the script

---
+++ gitcistats.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+OUTPUT_PREFIX="gitcistats"
+
+timestamp=$( date +%Y-%m-%d )
+output_file=$OUTPUT_PREFIX-$timestamp.html
+
+cat <<EOF > $output_file
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Top commiters - $timestamp</title>
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+</head>
+<body>
+<table class="pure-table">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Num commits</th>
+            <th>Author</th>
+        </tr>
+    </thead>
+<tbody>
+EOF
+
+git shortlog -s -n | awk '{ print "<tr><td>" FNR "</td><td>" $1 "</td><td>" $2" "$3 "</td></tr>" }
+     END   { print "</tbody></table>" }'  >> $output_file
+
+echo "</body>" >> $output_file
+echo "</html>" >> $output_file
